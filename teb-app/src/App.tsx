3b1bc103557ff4b6231d752efdf895a11fc8e7f5
Oskar Larsen <oskarla@stud.ntnu.no>
2025-11-13 20:06:22 +0100

    update standard colors

--- teb-app/src/App.tsx
+++ teb-app/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import './App.css'
@@ -9,7 +8,8 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow flex items-center justify-center">
-        <h1>Nettstedet er under konstant utvikling. Hele tiden liksom</h1>
+        <h1 className="bg-navy-dark">Jeg er kul</h1>
+        <h1 className="bg-teb-green-light">Nettstedet er under konstant utvikling. Hele tiden liksom</h1>
       </main>
       <Footer />
     </div>
